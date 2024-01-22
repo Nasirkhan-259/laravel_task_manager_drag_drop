0e6eafa4cff2b7af4682d67fa9893e8d9fe4c20c
Nasir <nasir.developer259@gmail.com>
2024-01-22 12:07:58 +0500

    some changes

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -4,7 +4,7 @@
 	<div class="container mt-3" ng-controller="projectCtrl">
 		<div class="row">
 			<div class="col-md-12 text-center">
-				<h3><a href="{{ URL::to('projects') }}">Checkout all projects</a></h3>
+				<h3><a href="{{ URL::to('projects') }}">All projects</a></h3>
 			</div>
 		</div>
 	</div>
